39b64d462b32325daf66f60ee171a703f65c10fe
StefanCla <201386@buas.nl>
2025-07-12 01:13:45 +0100

    Add initial project

---
+++ Source/ShovelJam2025/ShovelJam2025.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ShovelJam2025.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ShovelJam2025, "ShovelJam2025" );
